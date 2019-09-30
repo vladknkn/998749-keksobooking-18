df90a86d883b07ee6692fac747e4ce3dee60a1b0
vladknkn <vladknkn@yandex.ru>
2019-09-30 16:39:03 +0400

    Исправил ошибки и дополнил код

--- js/main.js
+++ js/main.js
@@ -5,33 +5,34 @@ var OFFER_CHECKINS = ['12:00', '13:00', '14:00'];
 var OFFER_CHECKOUTS = ['12:00', '13:00', '14:00'];
 var OFFER_FEATURES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
 var OFFER_PHOTOS = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
-var avatarNumber = [];
-var locationYNumber = [];
 
-for(var i = 1; i < 9; i++) {
-  var number = i;
-  avatarNumber.push(number);
-}
+function getRandomArbitrary (min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+};
 
-for (var a = 130; a < 631; a++) {
-  var numberY = a;
-  locationYNumber.push(numberY);
-}
+function getRandomNumber(amount) {
+  return Math.floor(Math.random() * amount);
+};
 
-var prices = [1000, 5000, 10000, 15000];
+function getAvatarNumbers (avatars) {
+  var avatarNumbers = [];
+  for (var j = 1; j <= avatars; j++) {
+    avatarNumbers.push(j);
+  } return avatarNumbers;
+};
 
-// ['', ''] | boolean
-function getRandomItem(itemsArr, isDelited) {
+var avatarNumbers = getAvatarNumbers(8);
+
+function getRandomItem(itemsArr, isDeleted) {
   var arrIndex = Math.floor(Math.random() * itemsArr.length); // 5
   var randomValue = itemsArr[arrIndex]; // '5 string'
-  if (isDelited) {
+  if (isDeleted) {
     itemsArr.splice(arrIndex, 1);
   }
   return randomValue;
 }
 
 function getArrayValues(itemsArr, count) {
-  // создаем новвый массив
   var itemsArrCopy = itemsArr.slice();
   var newArr = [];
   for (var i = 0; i < count && itemsArrCopy.length; i++) {
@@ -41,6 +42,13 @@ function getArrayValues(itemsArr, count) {
   return newArr;
 }
 
+function getLocationX () {
+  var map = document.querySelector('.map');
+  var width = map.clientWidth;
+  var locationX = Math.floor(Math.random() * width);
+  return locationX;
+};
+
 // avatarNumber[Math.floor(Math.random() * 8)]
 function getAvatarImgAddress(avatarNumber) {
   var str = (avatarNumber < 10) ? '0' : '';
@@ -48,52 +56,69 @@ function getAvatarImgAddress(avatarNumber) {
 }
 
 function getGeneratedAdvert() {
-  var rand = avatarNumber[Math.floor(Math.random() * 8)];
+  var rand = getRandomItem(avatarNumbers, true);
+  var locationX = getLocationX();
+  var locationY = getRandomArbitrary(130, 630);
   return {
     author: {
       avatar: getAvatarImgAddress(rand)
     },
 
     offer: {
-      title: '',
-      address: '',
-      price: getRandomItem(prices),
+      title: 'Заголовок',
+      address: locationX + ', ' + locationY,
+      price: getRandomArbitrary(1000, 15000),
       type: getRandomItem(OFFER_TYPES),
-      rooms: 1,
-      guests:  2,
+      rooms: 2,
+      guests:  4,
       checkin: getRandomItem(OFFER_CHECKINS),
       checkout: getRandomItem(OFFER_CHECKOUTS),
-      features: getArrayValues(OFFER_FEATURES, 3),
-      description: '',
-      photos: ['']
+      features: getArrayValues(OFFER_FEATURES, getRandomArbitrary(1, 6)),
+      description: 'Описание',
+      photos: getArrayValues(OFFER_PHOTOS, getRandomArbitrary(1, 3))
     },
 
     location: {
-      x: 10,
-      y: 20,
+      x: locationX,
+      y: locationY
     }
   };
 }
 
-/**
- * func name
- * @param {number} count - The title of the book.
- * @return {Array}  -  dsgsdgsdgsdg
- */
 function generateOffers(count) {
-  var tempAppr = [];
+  var offersArray = [];
   for (var i = 0; i < count; i++) {
-    tempAppr.push(getGeneratedAdvert());
+    offersArray.push(getGeneratedAdvert());
   }
-  return tempAppr;
+  return offersArray;
 }
 
-console.log(generateOffers(3))
+var offers = generateOffers(8);
+
+console.log(offers);
 
 
 var mapDialog = document.querySelector('.map');
 mapDialog.classList.remove('.map--faded');
 
-var similarWizardTemplate = document.querySelector('#pin')
-    .content
-    .querySelector('.map__pin');
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+
+function renderPins (advert) {
+  var pinElement = pinTemplate.cloneNode(true);
+  pinElement.style.fill = 'left: ' + advert.locationX + 'px; top: ' + advert.locationY + 'px;';
+  pinElement.src = advert.author.avatar;
+  pinElement.alt = advert.offer.title;
+  return pinElement;
+}
+
+console.log(renderPins(offers[1]));
+
+var mapPinsList = document.querySelector('.map__pins');
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < offers.length; i++) {
+  fragment.appendChild(renderPins(offers[i]));
+}
+
+var renderedPins = mapPinsList.appendChild(fragment);
+
